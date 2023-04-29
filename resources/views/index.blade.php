f6c170524e3f2117c23ac4d11db03526c71fb7a1
TheCreeeZ <tczdev@gmail.com>
2023-04-29 12:25:08 +0400

    Added front-end template part

---
+++ resources/views/index.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TCZ Forum</title>
+
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+</head>
+<body class="bg-light">
+
+  <nav class="navbar navbar-expand-md fixed-top navbar-dark bg-dark">
+    <a class="navbar-brand" href="/">TCZ Forum</a>
+
+    <div class="navbar-collapse offcanvas-collapse" id="navbarsExampleDefault" style="justify-content: flex-end;">
+      <a href="/profile" class="btn btn-outline-success my-2 my-sm-0" type="submit">Профиль</a>
+    </div>
+</nav>
+  <main role="main" class="container">
+    <div class="d-flex align-items-center p-3 my-3">
+    </div>
+
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+        <h1 class="display-4">TCZ Forum Main Page</h1>
+        <p class="lead">Всего тредов: N; Всего пользователей: D</p>
+      </div>
+    </div>
+
+    <div class="my-3 p-3 bg-white rounded box-shadow">
+      <h6 class="border-bottom border-gray pb-2 mb-0">Последняя активность</h6>
+          <a href="/thread">
+              <div class="media text-muted pt-3">
+                  <img alt="32x32" class="mr-2 rounded" style="width: 32px; height: 32px;" src="https://yt3.googleusercontent.com/WgW9_IWSVAbECTYYwMOannWIf2ddp4ceuvcm6DIsz-dIskqYQfWR-nX_1cxtpYD_RDHgl4skDeY=s900-c-k-c0x00ffffff-no-rj" data-holder-rendered="true">
+                  <p class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
+                  <strong class="d-block text-gray-dark">TheCreeeZ</strong>
+                  Template thread
+                  </p>
+              </div>
+          </a>
+    </div>
+
+  </main>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+Route::get('/thread', function () {
+    return view('thread');
 });
